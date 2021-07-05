0b544b956619c1c3557fd189a5b50b2cd3657e9f
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2021-07-05 12:29:53 +0200

    added render kwargs

--- pybulletgym/examples/example.py
+++
@@ -1,7 +0,0 @@
-import gym  # open ai gym
-import pybulletgym  # register PyBullet enviroments with open ai gym
-
-env = gym.make('InvertedPendulumMuJoCoEnv-v0')
-# env.render() # call this before env.reset, if you want a window showing the environment
-env.reset()
-env
